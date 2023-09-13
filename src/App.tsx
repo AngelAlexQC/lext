738386971854350adf35e694d348cbcfbce825dd
Ángel Quiroz <aquirozdev@gmail.com>
2023-09-12 21:04:33 -0500

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-//https://bun.sh
 export function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Lext - The next generation web framework for Bun</title>
+        <title>Lext - 🚀 The next generation web framework for Bun</title>
+        <link href="/styles.css" rel="stylesheet" />
       </head>
       <body>
-        <main>
-          <h1>Build your next generation web app with Lext</h1>
+        <main className="p-4 flex flex-col items-center gap-4">
+          <h1 className="text-3xl font-bold">
+            Build your next generation web app with Lext
+          </h1>
           <p>
             Lext is a new web framework for Bun. It is designed to be simple,
             flexible and fast.
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
 import { renderToReadableStream } from "react-dom/server";
 import { App } from "./App";
-
+import { execSync } from "child_process";
+const command = "bunx tailwindcss -i ./src/styles.css -o dist/styles.css";
+execSync(command, { stdio: "inherit" });
 Bun.serve({
-  async fetch() {
+  async fetch(req) {
+    const { pathname } = new URL(req.url);
+    if (pathname.startsWith("/styles.css")) {
+      const styles = Bun.file("./dist/styles.css");
+      return new Response(styles, {
+        headers: { "Content-Type": "text/css" },
+      });
+    }
+
     const stream = await renderToReadableStream(<App />);
     return new Response(stream, {
       headers: { "Content-Type": "text/html" },
