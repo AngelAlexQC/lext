6ce47f2b84727e59971eb962068c78f5fd24fb6e
Ángel Quiroz <aquirozdev@gmail.com>
2023-09-12 21:54:54 -0500

    feat: intial file router

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,26 @@
 import { renderToReadableStream } from "react-dom/server";
 import { compileStyles } from "./lib/compileStyles";
-import { FileSystemRouter } from "bun";
 
 await compileStyles();
 
-export default {
-  port: 3000,
-  async fetch(request: Request) {
-    const { pathname } = new URL(request.url);
-
-    if (pathname === "/styles.css") {
+Bun.serve({
+  async fetch(req) {
+    const { pathname } = new URL(req.url);
+    if (pathname.startsWith("/styles.css")) {
       const styles = Bun.file("./dist/styles.css");
-
       return new Response(styles, {
-        headers: {
-          "Content-Type": "text/css",
-        },
+        headers: { "Content-Type": "text/css" },
       });
     }
 
-    const router = new FileSystemRouter({
-      dir: import.meta.dir + "/pages",
-      style: "nextjs",
+    const folder = "pages";
+    const checkPath = pathname === "/" ? "/index" : pathname;
+    const path = `${folder}${checkPath}.tsx`;
+    const Page = await import(`./${path}`);
+
+    const stream = await renderToReadableStream(<Page.default />);
+    return new Response(stream, {
+      headers: { "Content-Type": "text/html" },
     });
-
-    const route = router.match(request);
-
-    const { default: Root } = await import(route?.filePath!);
-    return new Response(
-      await renderToReadableStream(<Root {...route?.params} />)
-    );
   },
-};
+});
---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Lext - 🚀 The next generation web framework for Bun</title>
+        <link href="/styles.css" rel="stylesheet" />
+      </head>
+      <body>
+        <main className="p-4 flex flex-col items-center gap-4">
+          <h1 className="text-3xl font-bold">About</h1>
+        </main>
+      </body>
+    </html>
+  );
+}
